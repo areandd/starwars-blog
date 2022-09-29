8b74981c847a88dbb59e876bdee00ad1fe073c18
Isaiah Desta <areandd@protonmail.com>
2022-09-29 16:19:49 -0400

    api intigration initiated.

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from '../store/appContext';
+import Card from "../component/Card";
 
 export const Home = () => {
+  const { store, actions } = useContext(Context);
+  console.log(store.characters);
   return (
-    <div >
-    testing
+    <div>
+      {store.characters.map((item, index) => {
+        return (
+          <Card item={item}/>
+        )
+
+      })}
     </div>
   );
 };
